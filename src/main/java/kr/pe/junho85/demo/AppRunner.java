d0e4b7db000e0a9c4ff287afde8a20d2f95d25a8
june.kim <junho85@gmail.com>
2020-07-05 18:50:15 +0900

    14.Validation 추상화

--- src/main/java/kr/pe/junho85/demo/AppRunner.java
+++ src/main/java/kr/pe/junho85/demo/AppRunner.java
@@ -9,18 +9,43 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Arrays;
 
 @Component
 public class AppRunner implements ApplicationRunner {
     @Autowired
     ApplicationContext resourceLoader;
 
+    @Autowired
+    Validator validator;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println(resourceLoader.getClass());
-        Resource resource = resourceLoader.getResource("file:///text.txt");
-//        Resource resource = resourceLoader.getResource("text.txt");
-        System.out.println(resource.getClass());
+//        System.out.println(resourceLoader.getClass());
+//        Resource resource = resourceLoader.getResource("file:///text.txt");
+////        Resource resource = resourceLoader.getResource("text.txt");
+//        System.out.println(resource.getClass());
 
+        System.out.println(validator.getClass());
+
+        Event event = new Event();
+        event.setLimit(-1);
+        event.setEmail("aaa2");
+        Errors errors = new BeanPropertyBindingResult(event, "event");
+
+        validator.validate(event, errors);
+
+        System.out.println(errors.hasErrors());
+
+        errors.getAllErrors().forEach(e -> {
+            System.out.println("====== error code =====");
+            Arrays.stream(e.getCodes()).forEach(System.out::println);
+            System.out.println(e.getDefaultMessage());
+        });
     }
 }
